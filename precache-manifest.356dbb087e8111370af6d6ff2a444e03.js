64b4402873fbb3ca11abab2f80215f05a72c37ea
[Github <[Github>
2022-08-30 15:21:15 +0900

    Updates

---
+++ precache-manifest.356dbb087e8111370af6d6ff2a444e03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b3eaeb5a854df57b0cd",
+    "url": "/movie_app_2022/static/css/main.44592867.chunk.css"
+  },
+  {
+    "revision": "5b3eaeb5a854df57b0cd",
+    "url": "/movie_app_2022/static/js/main.5b3eaeb5.chunk.js"
+  },
+  {
+    "revision": "67232fc44ae98e675d67",
+    "url": "/movie_app_2022/static/js/1.67232fc4.chunk.js"
+  },
+  {
+    "revision": "3363cc9578fa6c52fa57",
+    "url": "/movie_app_2022/static/js/runtime~main.3363cc95.js"
+  },
+  {
+    "revision": "2f02dd77ba08384a36129d4f3cd0b980",
+    "url": "/movie_app_2022/index.html"
+  }
+];
